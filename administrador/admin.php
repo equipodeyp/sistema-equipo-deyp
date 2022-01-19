147ac94a8983c5360e4c2bbadf8f39133a885e71
santijhon <santiagojhon037@gmail.com>
2022-01-19 10:06:48 -0600

    perfil administrador, vista admin de la tabla de expedientes

--- administrador/admin.php
+++ administrador/admin.php
@@ -3,6 +3,9 @@
 include("conexion.php");
 session_start ();
 $name = $_SESSION['usuario'];
+if (!isset($name)) {
+  header("location: ../logout.php");
+}
 $sentencia=" SELECT usuario, nombre, area, apellido_p, apellido_m FROM usuarios WHERE usuario='$name'";
 $result = $mysqli->query($sentencia);
 $row=$result->fetch_assoc();
@@ -12,40 +15,75 @@ $row=$result->fetch_assoc();
 <head>
   <script src="../js/botonatras.js"></script>
   <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
-  <title>UPSIPPED</title>
+  <title>SIPPSIPPED</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <script src="../js/jquery-3.1.1.min.js"></script>
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
   <link href="../css/bootstrap.min.css" rel="stylesheet">
   <link href="../css/bootstrap-theme.css" rel="stylesheet">
   <script src="../js/bootstrap.min.js"></script>
-  <link href="../css/jquery.dataTables.min.css" rel="stylesheet">
-  <script src="../js/jquery.dataTables.min.js"></script>
   <link rel="stylesheet" href="../css/cli.css">
-<script>
-  $(document).ready(function(){
-    $('#mitabla').DataTable({
-      "order": [[1, "asc"]],
-      "language":{
-      "lengthMenu": "Mostrar _MENU_ registros por pagina",
-      "info": "Mostrando pagina _PAGE_ de _PAGES_",
-        "infoEmpty": "No hay registros disponibles",
-        "infoFiltered": "(filtrada de _MAX_ registros)",
-        "loadingRecords": "Cargando...",
-        "processing":     "Procesando...",
-        "search": "Buscar:",
-        "zeroRecords":    "No se encontraron registros coincidentes",
-        "paginate": {
-          "next":       "Siguiente",
-          "previous":   "Anterior"
+  <!-- CSS personalizado -->
+  <link rel="stylesheet" href="../css/main2.css">
+  <!--datables CSS básico-->
+  <link rel="stylesheet" type="text/css" href="../datatables/datatables.min.css"/>
+  <!--datables estilo bootstrap 4 CSS-->
+  <link rel="stylesheet"  type="text/css" href="../datatables/DataTables-1.10.18/css/dataTables.bootstrap4.min.css">
+  <!--font awesome con CDN-->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+  <!-- datatables JS -->
+  <script type="text/javascript" src="../datatables/datatables.min.js"></script>
+  <!-- para usar botones en datatables JS -->
+  <script src="../datatables/Buttons-1.5.6/js/dataTables.buttons.min.js"></script>
+  <script src="../datatables/JSZip-2.5.0/jszip.min.js"></script>
+  <script src="../datatables/pdfmake-0.1.36/pdfmake.min.js"></script>
+  <script src="../datatables/pdfmake-0.1.36/vfs_fonts.js"></script>
+  <script src="../datatables/Buttons-1.5.6/js/buttons.html5.min.js"></script>
+<!-- SCRIPT PARA EL MANEJO DE LA TABLA -->
+  <script type="text/javascript">
+  $(document).ready(function() {
+      $('#example').DataTable({
+          language: {
+                  "lengthMenu": "Mostrar _MENU_ registros",
+                  "zeroRecords": "No se encontraron resultados",
+                  "info": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+                  "infoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+                  "infoFiltered": "(filtrado de un total de _MAX_ registros)",
+                  "sSearch": "Buscar:",
+                  "oPaginate": {
+                      "sFirst": "Primero",
+                      "sLast":"Último",
+                      "sNext":"Siguiente",
+                      "sPrevious": "Anterior"
+             },
+             "sProcessing":"Procesando...",
+              },
+          // para usar los botones
+          responsive: "true",
+          dom: 'Bfrtilp',
+          buttons:[
+        {
+          extend:    'excelHtml5',
+          text:      '<i class="fas fa-file-excel"></i> ',
+          titleAttr: 'Exportar a Excel',
+          className: 'btn btn-success'
         },
-      },
-      "bProcessing": true,
-      "bServerSide": true,
-      "sAjaxSource": "server_process.php"
-    });
+        {
+          extend:    'pdfHtml5',
+          text:      '<i class="fas fa-file-pdf"></i> ',
+          titleAttr: 'Exportar a PDF',
+          className: 'btn btn-danger'
+        },
+        {
+          extend:    'print',
+          text:      '<i class="fa fa-print"></i> ',
+          titleAttr: 'Imprimir',
+          className: 'btn btn-info'
+        },
+      ]
+      });
   });
-</script>
+  </script>
 </head>
 <body onload="nobackbutton();">
   <div class="contenedor">
@@ -53,7 +91,22 @@ $row=$result->fetch_assoc();
       <div class="logo text-warning">
       </div>
       <div class="user">
-        <img src="../image/USER.png" alt="" width="100" height="100">
+        <?php
+        $sentencia=" SELECT usuario, nombre, area, apellido_p, apellido_m, sexo FROM usuarios WHERE usuario='$name'";
+        $result = $mysqli->query($sentencia);
+        $row=$result->fetch_assoc();
+        $genero = $row['sexo'];
+
+        if ($genero=='mujer') {
+          echo "<img src='../image/mujerup.png' width='100' height='100'>";
+        }
+
+        if ($genero=='hombre') {
+          // $foto = ../image/user.png;
+          echo "<img src='../image/hombreup.jpg' width='100' height='100'>";
+        }
+        // echo $genero;
+         ?>
         <span class='user-nombre'>  <?php echo "" . $_SESSION['usuario']; ?> </span>
       </div>
       <nav class="menu-nav">
@@ -61,8 +114,9 @@ $row=$result->fetch_assoc();
     </div>
     <div class="main bg-light">
       <div class="barra">
-        <img src="../image/ups.png" alt="" width="1400" height="150">
-        </i></a>
+          <img src="../image/fiscalia.png" alt="" width="150" height="150">
+          <img src="../image/ups2.png" alt="" width="1400" height="70">
+          <img style="display: block; margin: 0 auto;" src="../image/ups3.png" alt="" width="1400" height="70">
       </div>
       <div class="container">
         <div class="row">
@@ -79,22 +133,68 @@ $row=$result->fetch_assoc();
           <a href="new_exp.php" class="btn btn-primary">Nuevo Expediente</a>
         </div>
         <br>
-        <div class="row table-responsive">
-          <table class="display" id="mitabla">
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>UNIDAD</th>
-                <th>SEDE</th>
-                <th>MUNICIPIO</th>
-                <th>FECHA</th>
-                <th>FOLIO EXPEDIENTE</th>
-                <th>DETALLES</th>
-              </tr>
-            </thead>
-            <tbody>
-            </tbody>
-          </table>
+        <!--Ejemplo tabla con DataTables-->
+        <div class="">
+            <div class="row">
+                    <div class="col-lg-12">
+                        <div class="table-responsive">
+                            <table id="example" class="table table-striped table-bordered" cellspacing="0" width="100%">
+                            <thead>
+                                <tr>
+                                    <th style="text-align:center">ID</th>
+                                    <th style="text-align:center">SEDE</th>
+                                    <th style="text-align:center">MUNICIPIO DE RADICACION</th>
+                                    <th style="text-align:center">FECHA RECEPCION</th>
+                                    <th style="text-align:center">FOLIO EXPEDIENTE</th>
+                                    <th style="text-align:center">PERSONAS</th>
+                                    <th style="text-align:center">VALIDACION</th>
+                                    <th style="text-align:center">DETALLES</th>
+
+                                </tr>
+                            </thead>
+                            <tbody>
+                              <?php
+                              $sql = "SELECT * FROM expediente";
+                              $resultado = $mysqli->query($sql);
+                              $row = $resultado->fetch_array(MYSQLI_ASSOC);
+                              $fol_exp =$row['fol_exp'];
+                              $tabla="SELECT * FROM expediente";
+                              $var_resultado = $mysqli->query($tabla);
+                              while ($var_fila=$var_resultado->fetch_array())
+                              {
+                                $fol_exp2=$var_fila['fol_exp'];
+                                $abc="SELECT count(*) as c FROM datospersonales WHERE folioexpediente='$fol_exp2'";
+                                $result=$mysqli->query($abc);
+                                if($result)
+                                {
+                                  while($row=mysqli_fetch_assoc($result))
+                                  {
+                                    echo "<tr>";
+                                    echo "<td style='text-align:center'>"; echo $var_fila['id']; echo "</td>";
+                                    echo "<td style='text-align:center'>"; echo $var_fila['sede']; echo "</td>";
+                                    echo "<td style='text-align:center'>"; echo $var_fila['municipio']; echo "</td>";
+                                    echo "<td style='text-align:center'>"; echo $var_fila['fecharecep']; echo "</td>";
+                                    echo "<td style='text-align:center'>"; echo $var_fila['fol_exp']; echo "</td>";
+                                    echo "<td style='text-align:center'>"; echo $row['c']; echo "</td>";
+                                    echo "<td style='text-align:center'>"; if ($var_fila['validacion'] == 'true') {
+                                      echo "<i class='fas fa-check'></i>";
+                                    }elseif ($var_fila['validacion'] == 'false') {
+                                      echo "<i class='fas fa-times'></i>";
+                                    } echo "</td>";
+                                    echo "<td style='text-align:center'><a href='modificar.php?id=".$var_fila['fol_exp']."'><span class='glyphicon glyphicon-folder-open'></span></a></td>";
+
+                                    echo "</tr>";
+
+                                  }
+
+                                }
+                              }
+                            ?>
+                            </tbody>
+                           </table>
+                        </div>
+                    </div>
+            </div>
         </div>
       </div>
     </div>
