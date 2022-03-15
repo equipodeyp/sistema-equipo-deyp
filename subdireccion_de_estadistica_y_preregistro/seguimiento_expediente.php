ea58a891f66cd75b9f22527ea0557d60ab3f989f
azael537 <azaelolivargarcia@gmail.com>
2022-03-15 11:51:00 -0600

    15/03/2022

--- subdireccion_de_estadistica_y_preregistro/seguimiento_expediente.php
+++ subdireccion_de_estadistica_y_preregistro/seguimiento_expediente.php
@@ -1099,7 +1099,7 @@ var diasVigencia;
 
     </script>
 
-<script type="text/javascript">
+<!-- <script type="text/javascript">
 var selectAnalisisMulti = document.getElementById('ANALISIS').value;
 function ocultarCampos() {
   if (selectAnalisisMulti === "" || selectAnalisisMulti === null ){
@@ -1357,7 +1357,7 @@ function ocultarCamposNoFormalizado() {
 
 }
 ocultarCamposNoFormalizado();
-</script>
+</script> -->
 
 </body>
 </html>
