a24a6e8ef25879cf53470d865acfb1ce9264b586
santijhon <santiagojhon037@gmail.com>
2022-03-17 11:00:59 -0600

    detalles expediente

--- subdireccion_de_estadistica_y_preregistro/detalles_expediente.php
+++ subdireccion_de_estadistica_y_preregistro/detalles_expediente.php
@@ -162,7 +162,7 @@
 					<div class="col-md-4 inputGroupContainer">
 						<div class="input-group">
 			      		<span class="input-group-addon"><i class="fas fa-calendar-check"></i></span>
-				  		<input name="fecha" type="text" class="form-control"  id="fecha"  placeholder="fecha" value="<?php echo date("d/m/Y", strtotime($row['fecharecep'])); ?>" disabled>
+				  		<input name="fecha" type="text" class="form-control"  id="fecha"  placeholder="fecha" value="<?php echo $row['fecharecep']; ?>" disabled>
 			    		</div>
 					</div>
 				</div>
