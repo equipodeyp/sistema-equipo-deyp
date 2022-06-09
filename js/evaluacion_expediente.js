d592af3d68451fcbb11cfebeec0451e8f6d1b64a
santijhon <santiagojhon037@gmail.com>
2022-06-09 11:34:06 -0500

    correcciones de evaluaciones de expedientes y personas

--- js/evaluacion_expediente.js
+++ js/evaluacion_expediente.js
@@ -1,6 +1,5 @@
 var tipo_convenio = document.getElementById('select_tipo_convenio').value;
 function disabledcamposevaexp() {
-  console.log(tipo_convenio);
   if (tipo_convenio !== '') {
     document.getElementById('select_tipo_convenio').disabled = true;
     document.getElementById('fecha_firma').disabled = true;
@@ -8,7 +7,24 @@ function disabledcamposevaexp() {
     document.getElementById('vigencia').disabled = true;
     document.getElementById('input_id_convenio').disabled = true;
     document.getElementById('textarea_observaciones').disabled = true;
+    document.getElementById('textobserv').style.display = "none";
     document.getElementById('enter').style.visibility = "hidden";
   }
 }
 disabledcamposevaexp();
+// desactivar todo cuando sea ACUERDO de CONCLUSION
+var analisis = document.getElementById('analisis_m').value;
+function activaracuerdo() {
+  // console.log(analisis);
+  if (analisis === 'ACUERDO DE CONCLUSION' || analisis === 'ACUERDO DE CANCELACION') {
+    document.getElementById('tconve').style.display = "none";
+    document.getElementById('ffirma').style.display = "none";
+    document.getElementById('finicio').style.display = "none";
+    document.getElementById('vigen').style.display = "none";
+    document.getElementById('fterm').style.display = "none";
+    document.getElementById('tconvenios').style.display = "none";
+    document.getElementById('textobserv').style.display = "none";
+    document.getElementById('enter').style.visibility = "hidden";
+  }
+}
+activaracuerdo();
