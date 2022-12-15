c8740fe02449e0b2f2817cb1157fc88424190413
santijhon <santiagojhon037@gmail.com>
2022-12-14 18:16:43 -0600

    correcciones de medidas de alojamiento temporal

--- administrador/tablas_estadistica/medidas_alojamiento.php
+++ administrador/tablas_estadistica/medidas_alojamiento.php
@@ -1,7 +1,7 @@
 <?php
 //
 $alotem = "SELECT DISTINCT id_persona FROM medidas
-WHERE medida = 'VIII. ALOJAMIENTO TEMPORAL' AND estatus != 'CANCELADA'";
+WHERE medida = 'VIII. ALOJAMIENTO TEMPORAL'";
 $ralotem = $mysqli->query($alotem);
 while ($falotem = $ralotem->fetch_assoc()) {
   $idsujeto = $falotem['id_persona'];
@@ -27,63 +27,54 @@ while ($falotem = $ralotem->fetch_assoc()) {
   $ralotem4 = $mysqli->query($alotem4);
   $falotem4 = $ralotem4->fetch_assoc();
 
-  // if ($falotem3['date_provisional'] === '0000-00-00') {
-  //   $fechaincio = $falotem3['date_definitva'];
-  // }else {
-  //   $fechaincio = $falotem3['date_provisional'];
-  // }
-  // $fechafinal = $falotem4['date_ejecucion'];
-  // $intvl = $fechaincio->diff($fechafinal);
-
-
-
-
   $alotem2 = "SELECT * FROM datospersonales
   WHERE id = '$idsujeto'";
   $ralotem2 = $mysqli->query($alotem2);
   while ($falotem2 = $ralotem2->fetch_assoc()) {
-    echo "<tr>";
-      echo "<td style='text-align:left'>"; echo $falotem2['folioexpediente']; echo "</td>";
-      echo "<td style='text-align:center'>"; echo $falotem2['identificador']; echo "</td>";
-      echo "<td style='text-align:center'>"; echo $fsuj2021['t']; echo "</td>";
-      echo "<td style='text-align:center'>"; echo $fsuj2022['t']; echo "</td>";
-      echo "<td style='text-align:center'>"; if ($falotem3['date_provisional'] === '0000-00-00') {
-        echo date("d/m/Y", strtotime($falotem3['date_definitva']));
-      }else {
-        echo date("d/m/Y", strtotime($falotem3['date_provisional']));
-      }  echo "</td>";
-      echo "<td style='text-align:center'>"; if ($falotem4['date_ejecucion'] === '0000-00-00') {
-        echo "en ejecucion";
-      }else {
-        echo date("d/m/Y", strtotime($falotem4['date_ejecucion']));
-      } echo "</td>";
-      echo "<td style='text-align:center'>";
-      if ($falotem3['date_provisional'] === '0000-00-00') {
-        $date11 = $falotem3['date_definitva'];
-      }else {
-        $date11 = $falotem3['date_provisional'];
-      }
-      if ($falotem4['date_ejecucion'] === '0000-00-00') {
-        $date22 = '2022-12-14';
-      }else {
-        $date22 = $falotem4['date_ejecucion'];
-      }
-       // $date11;
-       $date1 = '2022-06-01'; // pick up date
-    $date2 = '2022-12-01'; // return date
-    $datetime1 = new DateTime($date11);
-    $datetime2 = new DateTime($date22);
-    $interval = $datetime1->diff($datetime2);
-    echo $interval->y . " año, " . $interval->m." mes y ".$interval->d." dia";
-echo "<br />";
-// Total amount of days
-echo $interval->days . " dias ";
 
+      echo "<tr bgcolor="; if($falotem4['estatus'] === 'CANCELADA'){
+        echo 'yellow';
+      } else{
+        echo 'white';
+      } echo ">";
+        echo "<td style='text-align:center'>"; echo $falotem2['folioexpediente']; echo "</td>";
+        echo "<td style='text-align:center'>"; echo $falotem2['identificador']; echo "</td>";
+        echo "<td style='text-align:center'>"; echo $fsuj2021['t']; echo "</td>";
+        echo "<td style='text-align:center'>"; echo $fsuj2022['t']; echo "</td>";
+        echo "<td style='text-align:center'>"; if ($falotem3['date_provisional'] === '0000-00-00') {
+          echo date("d/m/Y", strtotime($falotem3['date_definitva']));
+        }else {
+          echo date("d/m/Y", strtotime($falotem3['date_provisional']));
+        }  echo "</td>";
+        echo "<td style='text-align:center'>"; if ($falotem4['date_ejecucion'] === '0000-00-00') {
+          echo "en ejecucion";
+        }else {
+          echo date("d/m/Y", strtotime($falotem4['date_ejecucion']));
+        } echo "</td>";
+        echo "<td style='text-align:center'>";
+        if ($falotem3['date_provisional'] === '0000-00-00') {
+          $date11 = $falotem3['date_definitva'];
+        }else {
+          $date11 = $falotem3['date_provisional'];
+        }
+        if ($falotem4['date_ejecucion'] === '0000-00-00') {
+          $date22 = '2022-12-14';
+        }else {
+          $date22 = $falotem4['date_ejecucion'];
+        }
+         // $date11;
+         $date1 = '2022-06-01'; // pick up date
+         $date2 = '2022-12-01'; // return date
+         $datetime1 = new DateTime($date11);
+         $datetime2 = new DateTime($date22);
+         $interval = $datetime1->diff($datetime2);
+         echo $interval->y . " año, " . $interval->m." mes y ".$interval->d." dia";
+         echo "<br />";
+         // Total amount of days
+         echo $interval->days . " dias";
+        echo "</td>";
+        echo "</tr>";
 
-
-
-      echo "</td>";
-      echo "</tr>";
   }
 }
 ?>
