3ebf6569c87826c48adf1389f2b60b14a2af8c3a
santijhon <santiagojhon037@gmail.com>
2023-01-19 18:38:54 -0600

    correcciones al calcular el tiempo que permanecen en alojamiento temporal perfil administrador

--- administrador/tablas_estadistica/medidas_alojamiento.php
+++ administrador/tablas_estadistica/medidas_alojamiento.php
@@ -77,7 +77,7 @@ while ($falotem = $ralotem->fetch_assoc()) {
           $date11 = $falotem3['date_provisional'];
         }
         if ($falotem4['date_ejecucion'] === '0000-00-00') {
-          $date22 = '2023-01-19';
+          $date22 = date('Y/m/d');        
         }else {
           $date22 = $falotem4['date_ejecucion'];
         }
