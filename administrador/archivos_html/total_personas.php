605d6edaaab291f718a06efab6a069e3fb4f6df3
santijhon <santiagojhon037@gmail.com>
2023-03-24 10:29:41 -0600

    estatus dentro del programa en estadistica total de personas del perfil administrador

--- administrador/archivos_html/total_personas.php
+++ administrador/archivos_html/total_personas.php
@@ -251,6 +251,7 @@ $row=$result->fetch_assoc();
                                           <th style="text-align:center">FECHA DESINCORPORACION</th>
                                           <th style="text-align:center">ESTATUS SUJETO PROGRAMA</th>
                                           <th style="text-align:center">RELACIONADO</th>
+                                          <th style="text-align:center">ESTATUS DENTRO DEL PROGRAMA</th>
                                       </tr>
                                   </thead>
                                   <tbody>
@@ -427,6 +428,7 @@ $row=$result->fetch_assoc();
                                       } echo "</td>";
                                       echo "<td style='text-align:center'>"; echo $fsuj['estatus']; echo "</td>";
                                       echo "<td style='text-align:center'>"; echo $fsuj['relacional']; echo "</td>";
+                                      echo "<td style='text-align:center'>"; echo $fsuj['estatusprograma']; echo "</td>";
                                       echo "</tr>";
                                       // echo "<td style='text-align:center'>";  echo "</td>";
                                     }
