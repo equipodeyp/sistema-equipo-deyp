308c6ba0fe297e211363c5d2d72ba1f12049acc9
santijhon <santiagojhon037@gmail.com>
2023-07-14 18:18:33 -0600

    correccion tabla 7 de personas propuestas del reporte mensual del perfil administrador estadistica

--- administrador/tablas_estadistica/tabla7_reporte_mensual.php
+++ administrador/tablas_estadistica/tabla7_reporte_mensual.php
@@ -10,12 +10,24 @@ $mesant = $meses[date('n')-2];
 $mesanterior = date('n')-1;
 $cantidaddiasanterior = cal_days_in_month(CAL_GREGORIAN, $mesanterior, $anioActual);
 $fecha_inicio = $anioActual."-01-01";
-$fecha_anterior = $anioActual."-".$mesanterior."-".$cantidaddiasanterior;
-$diamesinicio = $anioActual."-".$mesActual."-01";
-$diamesfin = $anioActual."-".$mesActual."-".$cantidadDias;
+$fecha_anterior = $anioActual."-".'0'.$mesanterior."-".$cantidaddiasanterior;
+$diamesinicio = $anioActual."-".'0'.$mesActual."-01";
+$diamesfin = $anioActual."-".'0'.$mesActual."-".$cantidadDias;
 $date_principio = $anioActual."-01-01";
 $date_termino = $anioActual."-12-31";
 ////////////////////////////////////////////////////////////////////////////////
+// echo "dia inicial---";
+// echo $fecha_inicio;
+// echo "<br>";
+// echo "fecha del mes anterior----";
+// echo $fecha_anterior;
+// echo "<br>";
+// echo "dia del mes de inicio-----";
+// echo $diamesinicio;
+// echo "<br>";
+// echo "dia final del mes actual-----";
+// echo $diamesfin;
+// echo "<br>";
 $calidad = "SELECT calidadpersona, COUNT(*) AS t FROM datospersonales
 INNER JOIN autoridad ON datospersonales.id = autoridad.id_persona
 WHERE autoridad.fechasolicitud BETWEEN '$date_principio' AND '$date_termino' AND datospersonales.relacional = 'NO'
@@ -27,13 +39,13 @@ while ($fcalidad = $rcalidad->fetch_assoc()) {
   //////////////////////////////////////////////////////////////////////////////
   $calant = "SELECT COUNT(*) as t FROM datospersonales
   INNER JOIN autoridad ON datospersonales.id = autoridad.id_persona
-  WHERE datospersonales.calidadpersona = '$namecalidad' AND datospersonales.relacional ='NO' AND autoridad.fechasolicitud BETWEEN '$fecha_inicio' AND '$fecha_anterior'";
+  WHERE datospersonales.calidadpersona = '$namecalidad' AND datospersonales.relacional ='NO' AND autoridad.fechasolicitud_persona BETWEEN '$fecha_inicio' AND '$fecha_anterior'";
   $rcalant = $mysqli->query($calant);
   $fcalant = $rcalant->fetch_assoc();
   //////////////////////////////////////////////////////////////////////////////
   $calreporte = "SELECT COUNT(*) as t FROM datospersonales
   INNER JOIN autoridad ON datospersonales.id = autoridad.id_persona
-  WHERE datospersonales.calidadpersona = '$namecalidad' AND datospersonales.relacional ='NO' AND autoridad.fechasolicitud BETWEEN '$diamesinicio' AND '$diamesfin'";
+  WHERE datospersonales.calidadpersona = '$namecalidad' AND datospersonales.relacional ='NO' AND autoridad.fechasolicitud_persona BETWEEN '$diamesinicio' AND '$diamesfin'";
   $rcalreporte = $mysqli->query($calreporte);
   $fcalreporte = $rcalreporte->fetch_assoc();
   //////////////////////////////////////////////////////////////////////////////
@@ -49,13 +61,13 @@ while ($fcalidad = $rcalidad->fetch_assoc()) {
 ////////////////////////////////////////////////////////////////////////////////
 $totalanterior = "SELECT COUNT(*) as t FROM datospersonales
 INNER JOIN autoridad ON datospersonales.id = autoridad.id_persona
-WHERE datospersonales.relacional ='NO' AND autoridad.fechasolicitud BETWEEN '$fecha_inicio' AND '$fecha_anterior'";
+WHERE datospersonales.relacional ='NO' AND autoridad.fechasolicitud_persona BETWEEN '$fecha_inicio' AND '$fecha_anterior'";
 $rtotalanterior = $mysqli->query($totalanterior);
 $ftotalanterior = $rtotalanterior->fetch_assoc();
 ////////////////////////////////////////////////////////////////////////////////
 $totalreporte = "SELECT COUNT(*) as t FROM datospersonales
 INNER JOIN autoridad ON datospersonales.id = autoridad.id_persona
-WHERE datospersonales.relacional ='NO' AND autoridad.fechasolicitud BETWEEN '$diamesinicio' AND '$diamesfin'";
+WHERE datospersonales.relacional ='NO' AND autoridad.fechasolicitud_persona BETWEEN '$diamesinicio' AND '$diamesfin'";
 $rtotalreporte = $mysqli->query($totalreporte);
 $ftotalreporte = $rtotalreporte->fetch_assoc();
 ////////////////////////////////////////////////////////////////////////////////
