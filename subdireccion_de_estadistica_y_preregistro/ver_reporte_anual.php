1bd3a27dbc5944dc63a04a9f87b0738dac06e596
santijhon <santiagojhon037@gmail.com>
2024-01-04 10:40:11 -0600

    actualizaon de reportes perfil estadistica y pre-registro

--- subdireccion_de_estadistica_y_preregistro/ver_reporte_anual.php
+++ subdireccion_de_estadistica_y_preregistro/ver_reporte_anual.php
@@ -332,7 +332,7 @@ a:focus {
                 <tr>
                   <td  width="1000px" height="80"> <button type="button" <a name="button" class="btn btn-secondary" style="width:1100px;">
                     <img src="../image/CALENDARIO/12 ANUAL.png" alt="" width="520px" height="90"> </button><br>
-                    <iframe src="../docs/REPORTES/ANUAL2022.pdf" width="1100" height="900"></iframe>
+                    <iframe src="../docs/REPORTES/ANUAL2023.pdf" width="1100" height="900"></iframe>
                   </td>
                 </tr>
               </tbody>
