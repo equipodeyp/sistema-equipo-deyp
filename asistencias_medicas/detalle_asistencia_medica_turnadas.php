c83baf153708bab336dcd5b1d4e7963244345589
azael537 <azaelolivargarcia@gmail.com>
2025-01-28 14:27:09 -0600

    28/01/2024 Actualizacion del apartado de asistencias turnadas

--- asistencias_medicas/detalle_asistencia_medica_turnadas.php
+++ asistencias_medicas/detalle_asistencia_medica_turnadas.php
@@ -283,12 +283,19 @@ $respuesta7=$resultado7->fetch_assoc();
                   </div>
 
                   <div>
-                    <table class="table table-bordered" width="100%" border="1" cellpadding="0" cellspacing="0" >
+                    <table class="table table-bordered" width="100%" border="1" cellpadding="0" cellspacing="0">
+                    <?php
+                    $originalDate = $respuesta6['fecha_asistencia'];
+                    // echo $originalDate;
+                    $date = date("d/m/Y", strtotime($originalDate));
+                    // echo $date;
+                    ?>
 
                       <tbody>
                         <tr>
+                        
                           <th style="text-align:left;">FECHA:</th>
-                          <td style="text-align:left; background-color: #fff;"><?php echo $respuesta5['fecha_asistencia']; ?></td>
+                          <td style="text-align:left; background-color: #fff;"><?php echo $date; ?></td>
                         </tr>
 
                         <tr>
