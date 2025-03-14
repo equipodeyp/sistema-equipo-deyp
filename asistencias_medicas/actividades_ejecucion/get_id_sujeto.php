8be10bacdedcaa79a94fc5af23804e61c60fe286
santijhon <santiagojhon037@gmail.com>
2025-03-14 11:33:44 -0600

    consulta de traslados preliminar solo tabla perfil de ejecucion de medidas

---
+++ asistencias_medicas/actividades_ejecucion/get_id_sujeto.php
@@ -0,0 +1,37 @@
+<?php
+
+$conexion=mysqli_connect('localhost','root','','sistemafgjem');
+
+$folio_expediente=$_POST['expediente'];
+$folio = $folio_expediente;
+
+// $folio = 'UPSIPPED/TOL/113/015/2022';
+// echo "<input value='$folio_expediente'/>";
+
+
+$select2 = "SELECT datospersonales.id, datospersonales.identificador
+FROM datospersonales
+WHERE datospersonales.folioexpediente = '$folio'
+ORDER BY datospersonales.identificador ASC";
+
+
+// echo "<span class='input-group-addon'><i class='fas fa-solid fa-id-card'></i></span>
+//         <select class='form-control' id='id_sujeto' name='id_sujeto' required>
+//         <option disabled selected value=''>SELECCIONE EL ID DEL SUJETO</option>";
+
+echo "
+<option disabled selected value=''>SELECCIONE EL ID DEL SUJETO</option>
+";
+$answer2 = mysqli_query($conexion, $select2);
+while($valores2 = $answer2->fetch_assoc()){
+        $resultado = $valores2['identificador'];
+        $id_sujeto = $valores2['id'];
+        echo "<option value='$id_sujeto'>$resultado</option>";
+
+}
+
+
+
+// echo "</select>";
+
+?>
