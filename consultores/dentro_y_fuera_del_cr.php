cafb3b03ca75492120b1e42b707bd9ba34e331a1
santijhon <santiagojhon037@gmail.com>
2025-05-28 10:34:12 -0600

    sujetos dentro de cp

--- consultores/dentro_y_fuera_del_cr.php
+++ consultores/dentro_y_fuera_del_cr.php
@@ -210,7 +210,7 @@ $('#fueraresguardo').DataTable({
                     <tbody>
                       <?php
                       $contador = 0;
-                      $sql = "SELECT * FROM datospersonales WHERE estatus = 'SUJETO PROTEGIDO' || estatus = 'PERSONA PROPUESTA' AND relacional = 'NO'";
+                      $sql = "SELECT * FROM datospersonales WHERE (estatus = 'SUJETO PROTEGIDO' || estatus = 'PERSONA PROPUESTA') AND relacional = 'NO'";
                       $resultado = $mysqli->query($sql);
                       while ($row = $resultado->fetch_array(MYSQLI_ASSOC)) {
                         $id_sujeto = $row['id'];
