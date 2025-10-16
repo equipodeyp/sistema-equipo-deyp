7d12ce2e33d09b9ed6b195799dcc973e29460ec4
santijhon <santiagojhon037@gmail.com>
2025-10-16 17:41:34 -0600

    primer tabla de reporte semanal sin errores

--- administrador/archivos_html/reporte_semanal.php
+++ administrador/archivos_html/reporte_semanal.php
@@ -32,11 +32,11 @@
                            $diaini = date( "j", $diainicio);
                            $diafinal = strtotime($fecha_fin);
                            $diafin = date( "j", $diafinal);
-
-                           echo "<br>";
-                           // echo '<h1 style="text-align:center">' ; echo "DEL 1 DE ENERO AL ".$diafinsemant. " DE ".$meses[date('n')-1]; echo '</h1>';
-                           echo "<br>";
-                           echo '<h1 style="text-align:center">' ; echo "<b>"; echo "REPORTE SEMANAL <br> DEL ".$diaini." AL ".$diafin. " DE ".$meses[date('n')-1]. " DEL ".date('Y'); echo "</b>"; echo '</h1>';
+                           if ($diaini > $diafin) {
+                            echo '<h1 style="text-align:center">' ; echo "REPORTE SEMANAL DEL ".$diaini." DE ".$meses[date('n')-1]." AL ".$diafin. " DE ".$meses[date('n')]. " DEL ".date('Y'); echo '</h1>';
+                           } else {
+                            echo '<h1 style="text-align:center">' ; echo "REPORTE SEMANAL DEL ".$diaini." AL ".$diafin. " DE ".$meses[date('n')-1]. " DEL ".date('Y'); echo '</h1>';
+                           }
                     break;
                     case "Monday"://LUNES
                            $fecha_inicio =  date("Y-m-d",strtotime($fecha_actual));
@@ -146,6 +146,43 @@
                     break;
                 }
                 ?>
+                <button class="btn btn-success" type="submit">GENERAR PDF</button><br><br><br>
+                <div class="row">
+                  <div class="col-lg-6">
+                    <div class="table-responsive">
+                      <table id="tabla1" border="1px" cellspacing="0" width="100%" bordered>
+                        <thead class="thead-dark">
+                          <tr>
+                            <th style="text-align:center" colspan="5">SOLICITUDES RECIBIDAS Y EXPEDIENTES INICIADOS</th>
+                          </tr>
+                          <tr>
+                            <th style="text-align:center" rowspan="2">CONCEPTO</th>
+                            <th style="text-align:center" colspan="2"><?php echo $year; ?></th>
+                            <th style="text-align:center" rowspan="2">TOTAL ACUMULADO</th>
+                          </tr>
+                          <tr>
+                            <th style="text-align:center"><?php echo "DEL 1 DE ENERO<br> AL ".$diafinsemant. " DE ".$meses[date('n')-1]; ?></th>
+                            <th style="text-align:center"><?php
+                              if ($diaini > $diafin) {
+                                echo "DEL ".$diaini." DE ".$meses[date('n')-1]." AL ".$diafin. " DE ".$meses[date('n')]. " DEL ".date('Y'); echo '</h1>';
+                              } else {
+                                  echo "DEL ".$diaini." AL ".$diafin. " DE ".$meses[date('n')-1]. " DEL ".date('Y'); echo '</h1>';
+                                }
+                             ?></th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <?php
+                          include("../administrador/tablas_estadistica/tabla1_reporte_semanal.php");
+                          ?>
+                        </tbody>
+                      </table>
+                      <br>
+                      <textarea name="name" rows="5" cols="87" placeholder="En caso de alguna  nota agregar aqui" style="resize: none;"></textarea>
+                    </div>
+                  </div>
+
+                </div>
               </form>
             <!-- </div> -->
 
