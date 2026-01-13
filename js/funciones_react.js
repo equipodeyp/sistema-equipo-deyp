e095985992336e7f8bc81e9cb1a63769a6471d14
santijhon <santiagojhon037@gmail.com>
2026-01-13 17:57:17 -0600

    add menu ul

--- js/funciones_react.js
+++ js/funciones_react.js
@@ -5,3 +5,17 @@ jQuery(document).ready(function(){
     jQuery(this).val(jQuery(this).val().replace(/[^0-9]/g, ''));
   });
 });
+
+
+function toggleSubmenu(element) {
+    var submenu = element.nextElementSibling;
+    var icon = element.querySelector('.fa-chevron-down');
+
+    if(submenu.style.display === "none") {
+        submenu.style.display = "block";
+        icon.style.transform = "rotate(180deg)";
+    } else {
+        submenu.style.display = "none";
+        icon.style.transform = "rotate(0deg)";
+    }
+}
