33e4c13c886c4c1e577cebfd2118728b408a4835
santijhon <santiagojhon037@gmail.com>
2026-03-19 17:56:05 -0600

    actualizacion menu bases de datos/planeacion/expediente

--- administrador/planeacion/expediente.php
+++ administrador/planeacion/expediente.php
@@ -37,6 +37,39 @@ $row=$result->fetch_assoc();
   <link rel="stylesheet" href="../../css/button_notification.css" type="text/css">
   <link href="../../datatables/datatablesv2026.min.css" rel="stylesheet">
   <script src="../../datatables/datatablesv2026.min.js"></script>
+  <script type="text/javascript">
+  $(document).ready(function() {
+      $('#example').DataTable({
+          language: {
+                  "lengthMenu": "Mostrar _MENU_ registros",
+                  "zeroRecords": "No se encontraron resultados",
+                  "info": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+                  "infoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+                  "infoFiltered": "(filtrado de un total de _MAX_ registros)",
+                  "sSearch": "Buscar:",
+                  "oPaginate": {
+                      "sFirst": "Primero",
+                      "sLast":"Último",
+                      "sNext":"Siguiente",
+                      "sPrevious": "Anterior"
+             },
+             "sProcessing":"Procesando...",
+              },
+          // para usar los botones
+          responsive: "true",
+          dom: 'Bfrtilp',
+          buttons:[
+        {
+          extend:    'excelHtml5',
+          text:      '<i class="fas fa-file-excel"></i> ',
+          titleAttr: 'Exportar a Excel',
+          className: 'btn color-btn-export-xls',
+          title:      'METAS ESTUDIOS'
+        },
+      ]
+      });
+  });
+  </script>
 </head>
 <body>
   <div class="contenedor">
